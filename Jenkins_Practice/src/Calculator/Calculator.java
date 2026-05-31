09a5c7ba2c8d7a2a67651d4998eec40356a009fb
mireat <162596683+So1ky@users.noreply.github.com>
2026-06-01 00:10:41 +0900

    first commit

--- Jenkins_Practice/src/Calculator/Calculator.java
+++ Jenkins_Practice/src/Calculator/Calculator.java
@@ -1,13 +1,5 @@
 package Calculator;
 
 public class Calculator {
-	
-	public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-        System.out.println("1 + 2 = " + add(1, 2));
-    }
 
-    static int add(int a, int b) {
-        return a + b;
-    }
 }
